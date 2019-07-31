e85ddc51317732b7c1f9699e6d8887c409c0c628
pangxie <pangxie@meili-inc.com>
2019-07-31 10:46:30 +0800

    160

--- leetcode/src/main/java/com/pangxie/server/leetcode/easy/intersectionoftwolinkedlists/IntersectionOfTwoLinkedListsV1.java
+++ leetcode/src/main/java/com/pangxie/server/leetcode/easy/intersectionoftwolinkedlists/IntersectionOfTwoLinkedListsV1.java
@@ -27,12 +27,52 @@ public class IntersectionOfTwoLinkedListsV1 {
         if (headA == null || headB == null) {
             return null;
         }
-        ListNode tempA=headA;
-        ListNode tempB=headB;
-        int count=0;
-        while(true){
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        int countA = 0;
+        int countB = 0;
+        while (true) {
+            if (tempA != null) {
+                tempA = tempA.next;
+                countA++;
+            }
+            if (tempB != null) {
+                tempB = tempB.next;
+                countB++;
+            }
+            if (tempA == null && tempB == null) {
+                break;
+            }
+        }
+        tempA = headA;
+        tempB = headB;
+        int maxLength = Math.max(countA, countB);
+        while (maxLength > 0) {
+            if (maxLength > countA) {
+                tempB = tempB.next;
+                maxLength--;
+                continue;
+            }
+            if (maxLength > countB) {
+                tempA = tempA.next;
+                maxLength--;
+                continue;
+            }
+            //先移除两者不相等
+            //然后逐步比较，相等才是前提
+            if (tempA == null || tempB == null) {
+                return null;
+            }
+            if (tempA.equals(tempB)) {
+                return tempA;
+            }
+            tempA = tempA.next;
+            tempB = tempB.next;
+            maxLength--;
 
         }
+        return null;
+
 
     }
 
